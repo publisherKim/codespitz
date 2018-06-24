65057dc19b4a4c181212714a4412381f10de0815
incorrectDeveloper <hdpgpp@gmail.com>
2018-06-24 22:48:21 +0900

    전략 팩턴

--- week03/GihubWithLoader.js
+++ week03/GihubWithLoader.js
@@ -7,13 +7,13 @@ const Github = class {
     const f = Github[id] = ({data:{content}}) => {
       delete Github[id];
       document.head.removeChild(s);
-      this._parser(content);  // 위임부분
+      this._parser(content, ...this._parser[1]);  // 위임부분
     };
     const s = document.createElement('script');
     s.src = `${this._base + path}?callbak=Github. ${id}`;
     document.head.appendChild(s);
   }
-  set parser(v) {this._parser = v;} // 위임객체 STARATEGY OBJECT: 실행지점 선택시 변하는 부분
+  setParser(f, ...arg) {this._parser = [f, arg];} // 위임객체 STARATEGY OBJECT: 실행지점 선택시 변하는 부분
 };
 
 Github._id = 0;
--- week03/Loader.js
+++ week03/Loader.js
@@ -3,11 +3,11 @@ const parseMd = v => {};
 const loader = new Github('hikaMaeng', 'codespitz75');
 
 // img 
-const img = v => el('#a').src = 'data:text/plain;base64,' + v;
-loader.parser = img;
+const img = (v, el) => el.src = 'data:text/plain;base64,' + v;
+loader.setParser(img, el('#a'));
 loader.load('xx.png');
 
 // md
-const md = v => el('#b').innerHTML = parseMD(v);
-loader.parser = md;
+const md = (v, el) => el.innerHTML = parseMD(v);
+loader.setParser = (md, el('#b'));
 loader.load('xx.md');
