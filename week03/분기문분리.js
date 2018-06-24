057084e3096c92624f4114e4ae64f31772c8ff70
incorrectDeveloper <hdpgpp@gmail.com>
2018-06-24 19:28:42 +0900

    분기문을 함수로 분리하는 경우

---
+++ week03/분기문분리.js
@@ -0,0 +1,15 @@
+const caseby = 1;
+// if (caseby == 1) {
+//   //
+// } else if (caseby == 2) {
+//   //
+// } else if (caseby == 3) {
+//   //
+// }
+if (caseby == 1) {
+  case1();
+} else if (caseby == 2) {
+  case2();
+} else if (caseby == 3) {
+  case3();
+}
